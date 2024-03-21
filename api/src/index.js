c0daa53381c41a0791a11f87a0d324f1f384bffe
Rafael Lemes <rafael.lemes@avmb.com.br>
2024-03-21 11:16:48 -0300

    fix: correção path para o arquivo index.html

--- api/src/index.js
+++ api/src/index.js
@@ -3,16 +3,17 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 app.get("/registration", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/index.html"));
+  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
 });
 
 app.post("/registration", (req, res) => {
-    console.log('req.body')
-    res.send({status: 201,msg: "Cadastro realizado com sucesso!"})
+  console.log("req.body");
+  res.send({ status: 201, msg: "Cadastro realizado com sucesso!" });
 });
 
 app.listen(port, () => {
-  console.log(`API Server runnnig on port port ${port}!`);
+  console.log(`API Server runnnig on port port http://localhost:${port}!`);
 });
